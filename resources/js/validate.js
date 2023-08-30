10f2dfce513c8f399bc52621b781cac78f36851e
usii-004 <usmansmooth004@gmail.com>
2023-08-30 02:28:10 +0100

    svg elem fixed, css error class added

--- resources/js/validate.js
+++ resources/js/validate.js
@@ -1,4 +1,10 @@
 const dayInput = document.getElementById('day-input');
 const monthInput = document.getElementById('month-input');
 const yearInput = document.getElementById('year-input');
+const submit = document.getElementById('svg-img');
 
+submit.addEventListener('click', () => {
+  if (dayInput.innerText == "") {
+    
+  }
+})

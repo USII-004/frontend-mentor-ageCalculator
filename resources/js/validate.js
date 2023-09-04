872f479d5c96e9031bdff06e0eff2a763485984f
usii-004 <usmansmooth004@gmail.com>
2023-09-04 11:09:01 +0100

    js function to validate empty input field

--- resources/js/validate.js
+++ resources/js/validate.js
@@ -1,38 +1,61 @@
-const dayInput = document.getElementById('day-input');
-const monthInput = document.getElementById('month-input');
-const yearInput = document.getElementById('year-input');
+/*====================== VARIABLES  ===================*/ 
 
-const dayLabel = document.getElementById('day-label')
-const monthLabel = document.getElementById('month-label')
-const yearLabel = document.getElementById('year-label')
+const dayInput = document.getElementById('day-input'),
+      monthInput = document.getElementById('month-input'),
+      yearInput = document.getElementById('year-input'),
 
-const dayErrorMsg = document.getElementById('day-error-mg')
-const monthErrorMsg = document.getElementById('month-error-msg')
-const yearErrorMsg = document.getElementById('year-error-msg')
+      dayLabel = document.getElementById('day-label'),
+      monthLabel = document.getElementById('month-label'),
+      yearLabel = document.getElementById('year-label'),
 
-const submit = document.getElementById('svg-img');
+      dayErrorMsg = document.getElementById('day-error-msg'),
+      monthErrorMsg = document.getElementById('month-error-msg'),
+      yearErrorMsg = document.getElementById('year-error-msg')
 
-submit.addEventListener('click', () => {
-  if (dayInput.value == "") {
-    dayLabel.classList.add(error__state__color);
-    dayInput.classList.remove('input__correct__state');
-    dayInput.classList.add('input__error__state');
-    dayErrorMsg.innerText = 'This field is required';
-    dayErrorMsg.classList.add('error__state__color');    
-  } else if (monthInput.value == "") {
-    monthLabel.classList.add(error__state__color);
-    monthInput.classList.remove('input__correct__state');
-    monthInput.classList.add('input__error__state');
-    monthErrorMsg.innerText = 'This field is required';
-    monthErrorMsg.classList.add('error__state__color');
-  } else if (yearInput.value == "") {
-    yearLabel.classList.add(error__state__color);
-    yearInput.classList.remove('input__correct__state');
-    yearInput.classList.add('input__error__state');
-    yearErrorMsg.innerText = 'This field is required';
-    yearErrorMsg.classList.add('error__state__color');
+      
+/*====================== EMPTY FIELD VERIFICATION ===================*/
+
+dayInput.addEventListener('change', () => {
+  if(dayInput.value == "") {
+    dayLabel.classList.add('error__state__color')
+    dayInput.classList.add('input__error__state')
+    dayErrorMsg.classList.add('error__state__color')
+    dayErrorMsg.innerText = 'This field is required'
+  }else {
+    dayLabel.classList.remove('error__state__color')
+    dayInput.classList.remove('input__error__state')
+    dayErrorMsg.classList.remove('error__state__color')
+    dayErrorMsg.innerText = ''
   }
-
 })
 
-// validation not working cus  I can't get my event listener to work on the svg
+monthInput.addEventListener('change', () => {
+  if(monthInput.value == "") {
+    monthLabel.classList.add('error__state__color')
+    monthInput.classList.add('input__error__state')
+    monthErrorMsg.classList.add('error__state__color')
+    monthErrorMsg.innerText = 'This field is required'
+  }else {
+    monthLabel.classList.remove('error__state__color')
+    monthInput.classList.remove('input__error__state')
+    monthErrorMsg.classList.remove('error__state__color')
+    monthErrorMsg.innerText = ''
+  }
+})
+
+yearInput.addEventListener('chnage', () => {
+  if(yearInput.value == "") {
+    yearLabel.classList.add('error__state__color')
+    yearInput.classList.add('input__error__state')
+    yearErrorMsg.classList.add('error__state__color')
+    yearErrorMsg.innerText = 'This field is required'
+  }else {
+    yearLabel.classList.remove('error__state__color')
+    yearInput.classList.remove('input__error__state')
+    yearErrorMsg.classList.remove('error__state__color')
+    yearErrorMsg.innerText = ''
+  }
+})
+
+
+/*====================== CALCULATE AGE ===================*/

e217861608cb274cb735fb4123cb204befb539a7
usii-004 <usmansmooth004@gmail.com>
2023-09-04 14:19:00 +0100

    age calculation function addedbut does not work as expected

--- resources/js/validate.js
+++ resources/js/validate.js
@@ -10,8 +10,56 @@ const dayInput = document.getElementById('day-input'),
 
       dayErrorMsg = document.getElementById('day-error-msg'),
       monthErrorMsg = document.getElementById('month-error-msg'),
-      yearErrorMsg = document.getElementById('year-error-msg')
+      yearErrorMsg = document.getElementById('year-error-msg'),
 
+      yearDisplay = document.getElementById('years-display'),
+      monthDisplay = document.getElementById('months-display'),
+      dayDisplay = document.getElementById('days-display'),
+
+      year = yearInput.value,
+      month = monthInput.value,
+      day = dayInput.value,
+      birthDate = `${year}-${month}-${day}`
+
+
+/*====================== CALCULATE AGE ===================*/
+// retrive input values
+
+
+function calculateAge(birthDate) {
+  const currentDate = new Date();
+  const birth = new Date(birthDate);
+
+  let years = currentDate.getFullYear() - birth.getFullYear();
+  let months = currentDate.getMonth() - birth.getMonth();
+  let days = currentDate.getDate() - birth.getDate();
+
+  // Check if the birth date has occurred this year
+  if (months < 0 || (months === 0 && days < 0)) {
+    years--;
+    months += 12; // Add 12 months to account for the previous year
+  }
+
+  // Calculate the difference in months and days
+  if (days < 0) {
+    const prevMonth = new Date(currentDate);
+    prevMonth.setMonth(currentDate.getMonth() - 1);
+    const daysInPrevMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    ).getDate();
+    days += daysInPrevMonth;
+    months--;
+  }
+  
+  return {
+    years,
+    months,
+    days,
+  };
+}
+      
       
 /*====================== EMPTY FIELD VERIFICATION ===================*/
 
@@ -43,7 +91,7 @@ monthInput.addEventListener('change', () => {
   }
 })
 
-yearInput.addEventListener('chnage', () => {
+yearInput.addEventListener('change', () => {
   if(yearInput.value == "") {
     yearLabel.classList.add('error__state__color')
     yearInput.classList.add('input__error__state')
@@ -54,8 +102,10 @@ yearInput.addEventListener('chnage', () => {
     yearInput.classList.remove('input__error__state')
     yearErrorMsg.classList.remove('error__state__color')
     yearErrorMsg.innerText = ''
+    
+    const age = calculateAge(birthDate)
+    yearDisplay.innerText = `${age.years}`
+    monthDisplay.innerText = `${age.months}`
+    dayDisplay.innerText = `${age.days}`
   }
 })
-
-
-/*====================== CALCULATE AGE ===================*/
